d13223a494494f1855518032ebe4562f9c414c9e
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-10-04 17:15:33 +0200

    adding a hover on portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -26,67 +26,71 @@ $(document).ready(function(){
 
 
 });
+$(document).ready(function(){
+  $("#work1").hover(function(){
+    $(".work1").slideDown("quick",function ()
+    {
+    });
+  });
+});
 
+$(document).ready(function(){
+  $("#work2").hover(function(){
+    $(".work2").slideDown("quick",function ()
+    {
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#work3").hover(function(){
+    $(".work3").slideDown("quick",function ()
+    {
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#work4").hover(function(){
+    $(".work4").fadeIn("quick",function ()
+    {
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#work5").hover(function(){
+    $(".work5").fadeToggle("quick",function ()
+    {
+    });
+  });
+});
+$(document).ready(function(){
+  $("#work6").hover(function(){
+    $(".work6").fadeIn("quick",function ()
+    {
+    });
+  });
+});
 
 
 $(document).ready(function(){
-  $("#work1").hover(function(){
-      $(".work1").css("opacity", 0.6);
-      $(".work1").fadeIn(800);
-      $(".work1").fadeOut(100).stop();
-
-
-     
-  });
-  $("#work2").hover(function(){
-      $(".work2").css("opacity", 0.6);
-      $(".work2").fadeIn(800);
-      $(".work2").fadeOut(100).stop();
-
-      
-  });
-  $("#work3").hover(function(){
-      $(".work3").css("opacity", 0.6);
-      $(".work3").fadeIn(800);
-      $(".work3").fadeOut(100).stop();
-
-      
-  });
-  $("#work4").hover(function(){
-      $(".work4").css("opacity", 0.6);
-      $(".work4").fadeIn(800);
-      $(".work4").fadeOut(100).stop();
-
-      
-  });
-  $("#work5").hover(function(){
-      $(".work5").css("opacity", 0.6);
-      $(".work5").fadeIn(800);
-      $(".work5").fadeOut(100).stop();
-
-      
-  });
-  $("#work6").hover(function(){
-      $(".work6").css("opacity", 0.6);
-      $(".work6").fadeIn(800);
-      $(".work6").fadeOut(100).stop();
-
-      
-  });
   $("#work7").hover(function(){
       $(".work7").css("opacity", 0.6);
       $(".work7").fadeIn(800);
       $(".work7").fadeOut(100).stop();
 
-      
   });
   $("#work8").hover(function(){
-      $(".work8").css("opacity", 0.6);
       $(".work8").fadeIn(800);
       $(".work8").fadeOut(50).stop();
 
-      
-  });
-
-
+      });
 });
+$(document).ready(function(){
+  $("#form").submit(function(){
+    var name=$("input#name").val();
+    alert(name+" " +"We have received your message");
+     
+  });
+})
