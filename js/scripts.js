8ea6ad34af355cb684d31805c89428c6823c9ec3
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-10-04 13:13:05 +0200

    modifying stylesheet file,javascript file and html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-    $("#design").click(function() {
-      $("#center").show();
-    });
+$(document).ready(function(){
+  $(".design").click(function(){
+      $(".design").toggle();
+      $("#icon1").toggle();
   });
-  
-  
+  $("#icon1").click(function(){
+      $(".design").toggle();
+      $("#icon1").toggle();
+  });
+  $("#devicon").click(function(){
+      $("#devicon").toggle();
+      $(".icon2").toggle();
+  });
+  $(".icon2").click(function(){
+      $("#devicon").toggle();
+      $(".icon2").toggle();
+  });
+  $(".product").click(function(){
+      $(".product").toggle();
+      $("#icon3").toggle();
+  });
+  $("#icon3").click(function(){
+      $(".product").toggle();
+      $("#icon3").toggle();
+  });
+
+
+});
